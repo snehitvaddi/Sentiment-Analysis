d351db0af3404bd6a0a5113550e167efcf4e9b8d
Snehit Vaddi <35267863+snehitvaddi@users.noreply.github.com>
2019-06-12 15:44:50 +0530

    Update and rename Sentiment Analysis - Supervised.ipynb to Sentiment Analysis - Supervised.py

---
+++ Sentiment Analysis - Supervised.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import text_normalizer as tn
+import model_evaluation_utils as meu
+
+np.set_printoptions(precision=2, linewidth=80)
+
+# # Load and normalize data
+dataset = pd.read_csv(r'movie_reviews.csv')
+
+# take a peek at the data
+
+print(dataset.head())
+reviews = np.array(dataset['review'])
+sentiments = np.array(dataset['sentiment'])
+
+# build train and test datasets
+train_reviews = reviews[:35000]
+train_sentiments = sentiments[:35000]
+test_reviews = reviews[35000:]
+test_sentiments = sentiments[35000:]
+
+# normalize datasets
+norm_train_reviews = tn.normalize_corpus(train_reviews)
+norm_test_reviews = tn.normalize_corpus(test_reviews)
+
+
+# # Traditional Supervised Machine Learning Models
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+# build BOW features on train reviews
+cv = CountVectorizer(binary=False, min_df=0.0, max_df=1.0, ngram_range=(1,2))
+cv_train_features = cv.fit_transform(norm_train_reviews)
+# build TFIDF features on train reviews
+tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1,2),
+                     sublinear_tf=True)
+tv_train_features = tv.fit_transform(norm_train_reviews)
+
+
+# transform test reviews into features
+cv_test_features = cv.transform(norm_test_reviews)
+tv_test_features = tv.transform(norm_test_reviews)
+
+print('BOW model:> Train features shape:', cv_train_features.shape, ' Test features shape:', cv_test_features.shape)
+print('TFIDF model:> Train features shape:', tv_train_features.shape, ' Test features shape:', tv_test_features.shape)
+
+## Model Training, Prediction and Performance Evaluation
+
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+
+lr = LogisticRegression(penalty='l2', max_iter=100, C=1)
+svm = SGDClassifier(loss='hinge', n_iter=100)
+
+
+# Logistic Regression model on BOW features
+lr_bow_predictions = meu.train_predict_model(classifier=lr, 
+                                             train_features=cv_train_features, train_labels=train_sentiments,
+                                             test_features=cv_test_features, test_labels=test_sentiments)
+meu.display_model_performance_metrics(true_labels=test_sentiments, predicted_labels=lr_bow_predictions,
+                                      classes=['positive', 'negative'])
+
+
+# Logistic Regression model on TF-IDF features
+lr_tfidf_predictions = meu.train_predict_model(classifier=lr, 
+                                               train_features=tv_train_features, train_labels=train_sentiments,
+                                               test_features=tv_test_features, test_labels=test_sentiments)
+meu.display_model_performance_metrics(true_labels=test_sentiments, predicted_labels=lr_tfidf_predictions,
+                                      classes=['positive', 'negative'])
+
+svm_bow_predictions = meu.train_predict_model(classifier=svm, 
+                                             train_features=cv_train_features, train_labels=train_sentiments,
+                                             test_features=cv_test_features, test_labels=test_sentiments)
+meu.display_model_performance_metrics(true_labels=test_sentiments, predicted_labels=svm_bow_predictions,
+                                      classes=['positive', 'negative'])
+
+
+svm_tfidf_predictions = meu.train_predict_model(classifier=svm, 
+                                                train_features=tv_train_features, train_labels=train_sentiments,
+                                                test_features=tv_test_features, test_labels=test_sentiments)
+meu.display_model_performance_metrics(true_labels=test_sentiments, predicted_labels=svm_tfidf_predictions,
+                                      classes=['positive', 'negative'])
+
